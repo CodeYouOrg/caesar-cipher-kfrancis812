09680a7cbf76b56cb5c2f272b667677c2b2a2e49
Kristin Francis <kfrancis812@gmail.com>
2024-08-13 19:55:56 -0400

    update 8.13.24

--- cipher.py
+++ cipher.py
@@ -1,1 +1,20 @@
-# add your code here
+alphabet='abcdefghijklmnopqrstuvwxyz'
+
+phrase = input("Please enter a sentence: ")
+
+phrase = phrase.lower()
+
+encrypted_phrase = ""
+
+shift = 5
+
+for char in phrase:
+    if char in alphabet:
+        index = alphabet.find(char)
+        index = (index + shift) % 26
+        char = alphabet[index]
+    encrypted_phrase += char
+
+print("The encrypted sentence is:" + encrypted_phrase)
+
+
